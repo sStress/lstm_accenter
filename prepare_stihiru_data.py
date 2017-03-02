94892d5568e53255bfe8d9cf75e5dbd0f9be3fb3
gyroklim <gyroklim@aklimenkov.TOPCON.COM>
2017-03-02 18:05:47 +0300

    - Some preliminary stuff

---
+++ prepare_stihiru_data.py
@@ -0,0 +1,46 @@
+from os import walk
+from bs4 import BeautifulSoup as BS
+import re
+
+def prepare_stihiru_data(file_name):
+    """
+    Simple function turning data from stihi.ru corpus provided by Orehov
+    into more easily handled format
+    """
+    soup = BS(open(file_name),'html.parser')
+    for div in soup('div'):
+        refined_text = ''
+        text = div.text
+        text = text.strip('\n')
+        lines = text.split('\n')
+        for line in lines[:-1]:
+            refined_text = refined_text + line.split('\t')[0] + '\n'
+        refined_text = refined_text.strip('\n')
+        #print('refined text:')
+        print(refined_text)
+        #print('end')
+
+    return refined_text
+
+files = []
+needed_files = []
+
+directory = '/home/gyroklim/documents/sstress/stihi_stressed_by_machine'
+for (_,_,filenames) in walk(directory):
+    files.extend(filenames)
+
+print('All files:')
+print(files)
+
+for file_ in files:
+    if file_.find('x') == 0:
+        needed_files.append(file_)
+
+print('Needed files')
+print(needed_files)
+
+final_file_name = 'final_file.txt'
+with open(final_file_name,'w') as final_file:
+    for file_ in needed_files:
+        data = prepare_stihiru_data(directory+'/'+file_)
+        final_file.write(data)
